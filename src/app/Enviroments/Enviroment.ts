71c5a2bfd1bf0297a47c22d0b69390e7143ec0b4
Brandon <oryzentsac.programador3@gmail.com>
2026-07-16 22:01:53 -0500

    Sprint 12

--- src/app/Enviroments/Enviroment.ts
+++ src/app/Enviroments/Enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8080'
-  apiUrl: 'https://c3b5-200-106-127-127.ngrok-free.app'
+  apiUrl: 'https://api.oryzentprueba.com'
 };
